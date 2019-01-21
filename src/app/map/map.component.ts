f5b5804a9198a728414439625edb0452b89f7f26
alaeddine thamine <alaeddine.thamine.prestataire@sfr.com>
2019-01-21 16:51:39 +0100

    commit 2

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,37 @@
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { LatLngBounds, MapsAPILoader } from '@agm/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.sass']
+})
+export class MapComponent implements OnInit {
+
+  @Input() nodes: {latitude: number, longitude: number}[];
+  latlngBounds: LatLngBounds;
+  height: number;
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader
+  ) { }
+
+  ngOnInit() {
+    // this.mapCenter();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.height = window.innerHeight;
+  }
+
+  mapCenter() {
+    // this.mapsAPILoader.load().then(() => {
+    //   this.latlngBounds = new window['google'].maps.LatLngBounds();
+    //   this.nodes.forEach((node: any) => {
+    //     this.latlngBounds.extend(new window['google'].maps.LatLng(node.latitude, node.longitude));
+    //   });
+    // });
+  }
+
+}
