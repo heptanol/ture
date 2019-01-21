ff5d17c66bd39ecf996138bebc4bfcd58bb6bef1
alaeddine thamine <alaeddine.thamine.prestataire@sfr.com>
2019-01-21 16:51:18 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'ture';
+  nodes = [
+    {latitude: 51.678418, longitude: 7.709007},
+    {latitude: 51.678510, longitude: 7.899007},
+    {latitude: 51.618418, longitude: 7.809007},
+    {latitude: 51.678418, longitude: 7.879007},
+    {latitude: 51.628418, longitude: 7.889007},
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyA3TWIbC60GZFx0_E0m2Fb7x7SSLVQ-kuw'
+    }),
+    AgmJsMarkerClustererModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
